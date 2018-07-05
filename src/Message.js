79ddf520ace251ccdf31d07006a2007f78cf8acf
heckenna <heckenna@rose-hulman.edu>
2018-07-05 10:55:27 -0400

    finish styles except for roomAnnouncement and submit button focus

--- src/Message.js
+++ src/Message.js
@@ -2,10 +2,24 @@ import React from 'react';
 
 const Message = (props) => {
     return(
-        <div className="Message">
-            {props.message.user.displayName}: {props.message.body}
+        <div className="Message" style={styles.message}>
+            {props.message.user.displayName}:
+            <div style={styles.details}> {props.message.body} </div>
         </div>
     )
+}
+
+const styles = {
+    message: {
+        display: "flex",
+        marginTop: "1rem",
+        padding: "0 1rem",
+    },
+      
+    details: {
+        flex: 1,
+        paddingLeft: "0.5rem",
+    }
 }
 
 export default Message;
--- src/MessageList.js
+++ src/MessageList.js
@@ -4,13 +4,30 @@ import Message from './Message';
 
 const MessageList = (props) => {
     return(
-        <div className="MessageList">
+        <div className="MessageList" style={styles.messageList}>
             { props.messages.map(msg  => (
-                    <Message message={msg} key={msg.id}/>
+                    <Message message={msg} key={msg.id} style={styles.h3}/>
                 )) 
             }
         </div>
     )
+}
+
+const styles = {
+    messageList: {
+        backgroundColor: "white",
+        flex: 1,
+        paddingBottom: "1rem",
+        overflowY: "scroll",
+    },
+      
+    roomAnnouncement: {
+        padding: "2rem 1rem",
+    },
+      
+    h3: {
+        fontSize: "1.5rem",
+    }
 }
 
 export default MessageList;
